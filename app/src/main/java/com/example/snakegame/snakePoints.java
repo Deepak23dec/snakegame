32d501757f4cb0a3a58814463c541fb33aaab1ba
ds1july <134129360+ds1july@users.noreply.github.com>
2023-05-27 09:04:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/snakegame/snakePoints.java
@@ -0,0 +1,30 @@
+package com.example.snakegame;
+
+public class snakePoints {
+
+
+    private int positionX, positionY;
+    public snakePoints(int positionX, int positionY){
+
+        this.positionX = positionX;
+        this.positionY = positionY;
+    }
+
+
+    public int getPositionX() {
+        return positionX;
+    }
+
+    public void setPositionX(int positionX) {
+        this.positionX = positionX;
+    }
+
+    public int getPositionY() {
+        return positionY;
+    }
+
+    public void setPositionY(int positionY) {
+        this.positionY = positionY;
+    }
+}
+
